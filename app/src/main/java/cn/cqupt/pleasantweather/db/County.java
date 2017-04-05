06c6953a3fa6c8c93d89e82e746415e1ca8143dc
shiwenming <shiwenming_sx@qiyi.com>
2017-04-05 10:21:15 +0800

    First commit

---
+++ app/src/main/java/cn/cqupt/pleasantweather/db/County.java
@@ -0,0 +1,18 @@
+package cn.cqupt.pleasantweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by shiwenming_sx on 2017/3/29.
+ */
+
+public class County extends DataSupport{
+
+    private int id;
+    private int cityId;
+    private String  weatherId;
+    private String countyName;
+
+
+
+}
